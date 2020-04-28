a7457b2c48221ab2afa5cc442248887e85b48295
ZYCGary <zyc.gary@gmail.com>
2020-04-28 21:04:22 +1000

    create model: bot, admin

---
+++ app/models/admin_model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const collectionName = 'admins'
+const schema = {
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    }
+}
+
+const AdminSchema = new mongoose.Schema(schema)
+module.exports = mongoose.model(collectionName, AdminSchema)
---
+++ app/models/bot_model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const collectionName = 'bots'
+const schema = {
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    }
+}
+
+const BotSchema = new mongoose.Schema(schema)
+module.exports = mongoose.model(collectionName, BotSchema)
