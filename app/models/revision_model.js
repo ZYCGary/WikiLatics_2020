a141f2ff50d09576ad7695da3d8491d0ba98d623
ZYCGary <zyc.gary@gmail.com>
2020-04-28 19:17:00 +1000

    delete unused model statics methods

--- app/models/revision_model.js
+++ app/models/revision_model.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose')
+
+const collectionName = 'revisions'
+const schema = {
+
+}
+
+const RevisionSchema = new mongoose.Schema(schema)
+module.exports = mongoose.model(collectionName, RevisionSchema)
